7b27749f55034a988184e3dc1c15f30b621ee250
TNS INDIA FOUNDATION <TNS INDIA FOUNDATION@LAPTOP-58EER3QF>
2023-04-18 16:20:37 +0530

    SimpleProgram

--- Basics/src/basics/Simple.java
+++ Basics/src/basics/Simple.java
@@ -1,10 +1,19 @@
 package basics;
 
 public class Simple {
-
+	
+	void add()
+	{
+		int a=10,b=20,c;
+		c=a+b;
+		System.out.println(c);
+	}
+	
 	public static void main(String[] args) {
 
 		System.out.println("Welcome");
+		Simple obj=new Simple();
+		obj.add();
 	}
 
 }
