0a79dbd1e40847755064d41230f3971ab3efbfc6
TNS INDIA FOUNDATION <TNS INDIA FOUNDATION@LAPTOP-58EER3QF>
2023-07-20 10:53:59 +0530

    JPAInheritance(Single_Table)

---
+++ JPASTInheritance/src/com/tns/jpastart/entities/InheritanceTest.java
@@ -0,0 +1,37 @@
+package com.tns.jpastart.entities;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+
+public class InheritanceTest {
+
+	public static void main(String[] args) {
+		
+		EntityManagerFactory factory = Persistence.createEntityManagerFactory("JPA-PU");
+		EntityManager em = factory.createEntityManager();
+		em.getTransaction().begin();
+		
+		//create one employee
+		Employee employee = new Employee();
+		employee.setName("Peter");
+		employee.setSalary(55000);
+		em.persist(employee);
+		
+		
+		//create one manager
+		Manager manager = new Manager();
+		manager.setName("Prajawal");
+		manager.setSalary(10000);
+		manager.setDepartmentName("Admin");
+		em.persist(manager);
+		
+		
+		
+		em.getTransaction().commit();
+		
+		System.out.println("Added one employee and manager to database.");
+		em.close();
+		factory.close();
+	}
+}
