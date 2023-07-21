e5115c1e691c4baf3135b76564f03df20fecf058
TNS INDIA FOUNDATION <TNS INDIA FOUNDATION@LAPTOP-58EER3QF>
2023-07-21 10:59:11 +0530

    JPA Inheritance (Table Per Class)

---
+++ JPATPCInheritance/src/com/tns/jpastart/entities/InheritanceTest.java
@@ -0,0 +1,36 @@
+package com.tns.jpastart.entities;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+
+public class InheritanceTest {
+
+	public static void main(String[] args) {
+		
+		EntityManagerFactory factory = Persistence.createEntityManagerFactory("JPA-PU");
+		EntityManager em = factory.createEntityManager();
+		em.getTransaction().begin();
+		
+		//create one employee
+		Employee employee = new Employee();
+		employee.setName("Roger");
+		employee.setSalary(45000);
+		em.persist(employee);
+		
+		//create one manager
+		Manager manager = new Manager();
+		manager.setName("Tom");
+		manager.setSalary(80000);
+		manager.setDepartmentName("IT");
+		em.persist(manager);
+		
+		
+		
+		em.getTransaction().commit();
+		
+		System.out.println("Added one employee and manager to database.");
+		em.close();
+		factory.close();
+	}
+}
