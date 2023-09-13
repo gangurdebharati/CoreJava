00ba91c5816e5409abf77ad07b7eebf90e6fc7d4
TNS INDIA FOUNDATION <TNS INDIA FOUNDATION@LAPTOP-58EER3QF>
2023-09-13 10:38:20 +0530

    Packages

---
+++ Packages/src/mypackage/B.java
@@ -0,0 +1,13 @@
+package mypackage;
+import pack.A;
+
+public class B extends A
+{
+	public static void main(String[] args) 
+	{
+		B obj=new B();
+		obj.msg();
+
+	}
+
+}
---
+++ Packages/src/pack/A.java
@@ -0,0 +1,10 @@
+package pack;
+
+ public class A 
+{
+	 protected void msg()
+	{
+		System.out.println("Hello students...");
+	}
+
+}
